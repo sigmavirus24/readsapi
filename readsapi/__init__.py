8012beb4b9bf4f677ad1213c0eaa39001a61672e
Cea Stapleton <chelsea.j.stapleton@gmail.com>
2015-10-03 09:36:57 -0500

    Basic db outline

--- readsapi/__init__.py
+++ readsapi/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask (__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/readsapi'
+db = SQLAlchemy(app)
 
 import readsapi.routes
